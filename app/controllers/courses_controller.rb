dcf252acdd7b0a51890f8bcc0803e2fddabc0a24
Maldon <maldonmeehan@gmail.com>
2016-09-08 19:34:16 -0700

    add delete function for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -37,6 +37,12 @@ class CoursesController < ApplicationController
     end
   end
 
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path
+  end
+
   private
     def course_params
       params.require(:course).permit(:name)
