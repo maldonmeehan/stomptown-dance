214ecbb69c7d345cabbbeb497517ec59c1f393cd
Maldon <maldonmeehan@gmail.com>
2016-09-08 19:21:01 -0700

    add create function for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,4 +9,24 @@ class CoursesController < ApplicationController
     render :show
   end
 
+  def new
+    @course = Course.new
+    render :new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to  courses_path
+    else
+      render :new
+    end
+  end
+
+private
+  def course_params
+    params.require(:course).permit(:name)
+  end
+
+
 end
