15dfe8ad0be04efd85b4b32568be9b941fcd6570
Maldon <maldonmeehan@gmail.com>
2016-09-09 11:43:55 -0700

    add create function for dances

--- app/controllers/dances_controller.rb
+++ app/controllers/dances_controller.rb
@@ -9,5 +9,23 @@ class DancesController < ApplicationController
     render :show
   end
 
+  def new
+    @dance = Dance.new
+    render :new
+  end
+
+  def create
+    @dance = Dance.new(dance_params)
+    if @dance.save
+      redirect_to  dances_path
+    else
+      render :new
+    end
+  end
+
+private
+  def dance_params
+    params.require(:dance).permit(:name)
+  end
 
 end
