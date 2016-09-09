f7fb03a9437bd4785d84cbfd868f2acdf89addd4
Maldon <maldonmeehan@gmail.com>
2016-09-09 14:11:57 -0700

    add update and delete for instructions

--- app/controllers/instructions_controller.rb
+++ app/controllers/instructions_controller.rb
@@ -14,6 +14,29 @@ class InstructionsController < ApplicationController
     end
   end
 
+  def edit
+    @dance = Dance.find(params[:dance_id])
+    @instruction = Instruction.find(params[:id])
+  end
+
+  def update
+    @dance = Dance.find(params[:dance_id])
+    @instruction = Instruction.find(params[:id])
+    if @instruction.update(instruction_params)
+      flash[:notice] = "Instruction successfully eddited!"
+      redirect_to dance_path(@instruction.dance)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @instruction = Instruction.find(params[:id])
+    @instruction.destroy
+    flash[:notice] = "Instructions successfully deleted!"
+    redirect_to dance_path(@instruction.dance)
+  end
+
 private
   def instruction_params
     params.require(:instruction).permit(:number, :move, :call, :instruction, :bars)
