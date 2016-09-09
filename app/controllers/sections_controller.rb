569dd9bc3cace90a7154d3449e057f5e09fd8294
Maldon <maldonmeehan@gmail.com>
2016-09-09 07:21:06 -0700

    add update function for sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,5 @@
 class SectionsController < ApplicationController
+
   def new
     @course = Course.find(params[:course_id])
     @section = @course.sections.new
@@ -14,8 +15,23 @@ class SectionsController < ApplicationController
     end
   end
 
-private
-  def section_params
-    params.require(:section).permit(:name)
+  def edit
+    @course = Course.find(params[:course_id])
+    @section = Section.find(params[:id])
   end
+
+  def update
+    @course = Course.find(params[:course_id])
+    @section= Section.find(params[:id])
+    if @section.update(section_params)
+      redirect_to course_path(@section.course)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def section_params
+      params.require(:section).permit(:name)
+    end
 end
