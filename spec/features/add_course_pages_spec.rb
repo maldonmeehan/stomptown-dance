c3d942456fb6c810ea5390edffc968ab909785ec
Maldon <maldonmeehan@gmail.com>
2016-09-09 08:05:12 -0700

    add capybara and passing intergration test for add a course process and gives error when no name is entered

---
+++ spec/features/add_course_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "the add a course process" do
+  it "adds a new course" do
+    visit courses_path
+    click_link 'New Course'
+    fill_in 'Name', :with => 'Course One'
+    click_on 'Create Course'
+    expect(page).to have_content 'Courses'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_course_path
+    click_on 'Create Course'
+    expect(page).to have_content 'errors'
+  end
+end
