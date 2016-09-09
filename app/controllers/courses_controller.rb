96747d4b4e883fc8fdba88d653074ed3d28661e3
Maldon <maldonmeehan@gmail.com>
2016-09-08 19:13:23 -0700

    add view for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,10 @@ class CoursesController < ApplicationController
     @courses = Course.all
     render :index
   end
+
+  def show
+    @course = Course.find(params[:id])
+    render :show
+  end
+
 end
