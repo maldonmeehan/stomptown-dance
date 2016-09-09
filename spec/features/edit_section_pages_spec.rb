fc5f793c293dffd0f2c80725cc7d7a8719e41c52
Maldon <maldonmeehan@gmail.com>
2016-09-09 08:14:38 -0700

    add passing intergration spec for edit a section process and gives error when no name is entered

---
+++ spec/features/edit_section_pages_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "the edit a section process" do
+  it "edits a section" do
+    course = Course.create(:name => 'Course One')
+    section = Section.create(:name => 'Section One', :course_id => course.id)
+    visit course_path(course)
+    click_on 'Edit'
+    fill_in 'Name', :with => 'Section Two'
+    click_on 'Update Section'
+    expect(page).to have_content 'Section Two'
+  end
+
+  it "gives error when no name is entered" do
+    course = Course.create(:name => 'Course One')
+    section = Section.create(:name => 'Section One', :course_id => course.id)
+    visit course_path(course)
+    click_on 'Edit'
+    fill_in 'Name', :with => ''
+    click_button 'Update Section'
+    expect(page).to have_content 'errors'
+  end
+end
