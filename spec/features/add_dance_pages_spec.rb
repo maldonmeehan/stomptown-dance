34942a9e8282599c59e6436042593cc8cab2cd46
Maldon <maldonmeehan@gmail.com>
2016-09-09 14:19:59 -0700

    add passing spec for add a dance process and gives error when no name is entered

---
+++ spec/features/add_dance_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "the add a dance process" do
+  it "adds a new dance" do
+    visit dances_path
+    click_link 'New Dance'
+    fill_in 'Name', :with => 'Dance One'
+    click_on 'Create Dance'
+    expect(page).to have_content 'Dance successfully added!'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_dance_path
+    click_on 'Create Dance'
+    expect(page).to have_content 'errors'
+  end
+end
