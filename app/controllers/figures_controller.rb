4637b84678a46724ce752cd7350013695dd0610d
Maldon <maldonmeehan@gmail.com>
2016-09-15 05:59:34 -0700

    add delete and update for figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,6 +15,29 @@ class FiguresController < ApplicationController
     end
   end
 
+  def edit
+    @dance = Dance.find(params[:dance_id])
+    @figure = Figure.find(params[:id])
+  end
+
+  def update
+    @dance = Dance.find(params[:dance_id])
+    @figure = Figure.find(params[:id])
+    if @figure.update(figure_params)
+      flash[:notice] = "Figre successfully edited!"
+      redirect_to dance_path(@figure.dance)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @figure = Figure.find(params[:id])
+    @figure.destroy
+    flash[:notice] = "Figure successfully deleted!"
+    redirect_to dance_path(@figure.dance)
+  end
+
   private
   def figure_params
     params.require(:figure).permit(:number, :title, :tune, :bar_total)
