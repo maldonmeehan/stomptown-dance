f9d3de5aa51dbf5d32f3aa4971fac95be2d1401d
Maldon <maldonmeehan@gmail.com>
2016-09-22 21:21:29 -0700

    end of night commit

--- spec/features/delete_instruciton_pages_spec.rb
+++ spec/features/delete_instruciton_pages_spec.rb
@@ -5,7 +5,7 @@
 #     user = FactoryGirl.create(:user)
 #     login_as(user, :scope => :user, :run_callbacks => false)
 #     dance = Dance.create(:name => 'Dance One')
-#     instruction = Instruction.create(:number => '1', :dance_id => dance.id)
+#     instruction = Instruction.create(:number => '1')
 #     visit dance_path(dance)
 #     click_on "Delete Instructions"
 #     expect(page).to have_content 'Instructions successfully deleted!'
